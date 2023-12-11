206b4f9591cefd2a51eed698661ba0d38ac109df
RafiAntaresa <145951611+RafiAntaresa@users.noreply.github.com>
2023-12-11 09:25:08 +0700

    Delete pantasin-api directory

--- pantasin-api/middleware/error.js
+++
@@ -1,9 +0,0 @@
-const errorHandler = (err, req, res, next) => {
-  let error = { ...err };
-  error.message = err.message;
-  res.status(error.statusCode || 500).json({
-    success: false,
-    message: error.message || "Server error",
-  });
-};
-module.exports = errorHandler;
